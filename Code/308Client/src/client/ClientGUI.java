920c6201ffc7f3ba55512c1e42ef8c5b8a06c813
matrixpeckham@gmail.com <matrixpeckham@gmail.com@1aaa48ef-6dfb-fb0f-bf95-efe5b36865b2>
2010-10-24 23:43:53 +0000

    Made code folder into an eclipse workspace, and laid some GUI groundwork, so far just the Client and ClientGUI are partially done.

---
+++ Code/308Client/src/client/ClientGUI.java
@@ -0,0 +1,35 @@
+package client;
+
+import javax.swing.JFrame;
+/**
+ * Main Frame, and starting point of the 
+ * client program
+ * @author Bill
+ *
+ */
+public class ClientGUI extends JFrame {
+
+	private Client networking;
+	private int permissions;
+	//TODO add screens when implemented
+	/**
+	 * Constructor, initialized and show
+	 */
+	public ClientGUI(){
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		init();
+		this.setVisible(true);
+	}
+	
+	private void init(){
+		
+	}
+	/**
+	 * Main Method, doesn't handle arguments (yet?)
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		new ClientGUI();
+	}
+
+}
